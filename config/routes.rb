a477eaa6e80dc1eaf5defd7b5f10326b8707a9d1
vivek_decorpot <vivek@decorpot.com>
2021-05-25 12:41:34 +0530

    Route change.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "static_pages#landing_page"
   get 'static_pages/landing_page'
   get 'static_pages/privacy_policy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
